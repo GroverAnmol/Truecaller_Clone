206a901ddfc0d157b093aa160a65c7e9fe380c3c
GroverAnmol <groveranmol369@gmail.com>
2024-03-29 19:53:08 +0530

    Initial commit

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:untitled35/screens/contacts_screen.dart';
+import 'package:untitled35/screens/recent_call_log_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+  @override
+  State<TabsScreen> createState() {
+    return _TabsScreenState();
+  }
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = const RecentsCallLogScreen();
+    if (_selectedPageIndex == 1) {
+      activePage = const ContactsScreen();
+    }
+
+    return Scaffold(
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedPageIndex,
+        onTap: _selectPage,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.list_rounded), label: 'Recents'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.contacts_rounded), label: 'Contacts'),
+        ],
+      ),
+    );
+  }
+}
